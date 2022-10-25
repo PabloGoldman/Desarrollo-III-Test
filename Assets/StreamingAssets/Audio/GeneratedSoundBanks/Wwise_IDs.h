2f4b5f41282e0957f1ecabe119d4543cd2516c02
julianmazzeo <j.mzz1956@gmail.com>
2022-10-25 14:22:46 -0300

    Wwise-integration

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_1ERSALTO_FOX = 2541583543U;
+        static const AkUniqueID PLAY_2DOSALTO_FOX = 1282247140U;
+        static const AkUniqueID PLAY_AMBIENTE = 94063951U;
+        static const AkUniqueID PLAY_ATAQUE_ENEMIGO = 3977950446U;
+        static const AkUniqueID PLAY_ATAQUE_TK = 3390788167U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_FOOTSTEPS_ENEMIGO = 3697655312U;
+        static const AkUniqueID PLAY_GAMEPLAY4 = 2811753234U;
+        static const AkUniqueID PLAY_MUERTE_ENEMIGO = 715145019U;
+        static const AkUniqueID PLAY_SALTO_TK = 2123649583U;
+        static const AkUniqueID STOP_FOOTSTEPS_ENEMIGO = 3318741046U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FS_FOX = 795715770U;
+                static const AkUniqueID FS_TK = 3866151752U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+        namespace HURTS
+        {
+            static const AkUniqueID GROUP = 3535913461U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID HURT_FOX = 1580675200U;
+                static const AkUniqueID HURT_TK = 3582750806U;
+            } // namespace SWITCH
+        } // namespace HURTS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
