045cbbd22030e5611e52b9e55669d63a19dbad9a
sebastian <sebbastian.gc@gmail.com>
2022-10-27 02:52:29 -0300

    Added Spawn enemies when player die

--- Assets/Scripts/SpawEnemies/SpawnController.cs
+++ Assets/Scripts/SpawEnemies/SpawnController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
    [SerializeField] private List<EnemyData> enemies;
-   
 
    private void OnEnable()
    {
       SpawnPoint.OnSpawn += Spawn;
+      PlayerManager.OnHit += SpawnWhitPlayer;
    }
 
    private void OnDisable()
    {
       SpawnPoint.OnSpawn -= Spawn;
+      PlayerManager.OnHit += SpawnWhitPlayer;
    }
    
    private void Spawn()
@@ -21,6 +22,15 @@ public class SpawnController : MonoBehaviour
       foreach (var e in enemies)
       {
          if (e.ReSpawn) e.Spawn();
+      }
+   }
+
+   private void SpawnWhitPlayer(float life)
+   {
+      if (life <= 0)
+      {
+         Debug.Log("spawn in player ");
+         Spawn();
       }
    }
 
