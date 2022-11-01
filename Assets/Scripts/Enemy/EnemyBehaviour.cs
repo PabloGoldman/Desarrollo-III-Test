104d9c3b905aecbad58496c85e0eba09771065ed
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-11-01 18:43:38 -0300

    Added enemy footstep sound WIP

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,14 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
     [SerializeField] private TypeOfMovement typeOfMovement;
     [SerializeField] private TypeOfAttack  typeOfAttack;
 
+    protected bool isFootsteping = false;
+
     private void Awake()
     {
        SetTypeOfMovement();
         SetTypeOfAttack();
+    }
+
+    private void Update()
+    {
+        if (!isFootsteping)
+        {
+            StartCoroutine(TriggerFootstepCoroutine());
+        }
+    }
+
+    protected IEnumerator TriggerFootstepCoroutine()
+    {
+        isFootsteping = true;
+        AkSoundEngine.PostEvent("Play_FS_TK", gameObject);
+        yield return new WaitForSeconds(0.3f);
+        isFootsteping = false;
     }
 
     private void SetTypeOfMovement()
