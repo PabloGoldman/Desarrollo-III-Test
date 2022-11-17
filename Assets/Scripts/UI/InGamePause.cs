c635696d2103a74d8808ac507373d8555cd83a15
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-11-17 10:11:44 -0300

    Added inGamePause WIP

---
+++ Assets/Scripts/UI/InGamePause.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class InGamePause : MonoBehaviour
+    {
+        [SerializeField] GameObject inGamePause;
+        [SerializeField] GameObject optionsMenu;
+
+        bool inPause = false;
+
+        private void Awake()
+        {
+            optionsMenu = transform.Find("OptionsPanel").gameObject;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!inPause)
+                {
+                    Pause();
+                }
+                else
+                {
+                    Resume();
+                }
+            }
+        }
+
+        public void Pause()
+        {
+            Time.timeScale = 0;
+            inGamePause.SetActive(true);
+            inPause = true;
+        }
+
+        public void Resume()
+        {
+            Time.timeScale = 1;
+            inGamePause.SetActive(false);
+            optionsMenu.SetActive(false);
+            inPause = false;
+        }
+
+        public void ReturnToProfile()
+        {
+            Resume();
+            //GameManager.instance.ChangeToMenu();
+        }
+    }
+}
+
