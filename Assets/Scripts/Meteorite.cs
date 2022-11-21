5279722b6632d17e390809d700b5877cc2ab89b3
sebastian <sebbastian.gc@gmail.com>
2022-11-21 14:46:28 -0300

    Added Feedback  meteor damage

--- Assets/Scripts/Meteorite.cs
+++ Assets/Scripts/Meteorite.cs
@@ -6,11 +6,12 @@ public class Meteorite : MonoBehaviour, IDamageable
     [SerializeField]private float timeToEnd;
     [SerializeField]private float life;
     [SerializeField]private GameObject FinalScreen;
-    
+    [SerializeField]private  ParticleSystem hurt;
 
-   public void TakeDamage(float damage)
+    public void TakeDamage(float damage)
    {
        life -= damage;
+       hurt.Play();
        if (life <= 0)
        {
            AkSoundEngine.PostEvent("Stop_Musica", gameObject);
