ec9871f8da299a57bb228a56c4236e4379f8e47b
sebastian <sebbastian.gc@gmail.com>
2022-11-29 11:03:54 -0300

    Added at the end of the credits change to menu

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -1,14 +1,14 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    [SerializeField] private float timeToExit;
     private void Awake()
     {
         Cursor.visible = true;
+        Invoke(nameof(Back),timeToExit);
     }
-
     public void Back()
     {
         AkSoundEngine.PostEvent("Play_UI_ENTER1", gameObject);
