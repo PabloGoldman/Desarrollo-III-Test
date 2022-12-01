850d36b1b0584e67404953546163021a9ef173ce
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-11-30 23:33:59 -0300

    Updated slider wwise behaviour

--- Assets/Scripts/UI/AudioSettings.cs
+++ Assets/Scripts/UI/AudioSettings.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace Game
 {
@@ -8,6 +9,34 @@ namespace Game
         private const string volumeKey = "Volumen_Musica";
         private const string sfxKey = "Volumen_SFX";
         private const string masterKey = "Volumen_Master";
+
+        public Slider thisSlider;
+        public float masterVolume;
+        public float musicVolume;
+        public float SFXVolume;
+
+        public void SetSpecificVolume(string whatValue)
+        {
+            float sliderValue = thisSlider.value;
+
+            switch (whatValue)
+            {
+                case "Master":
+                    masterVolume = thisSlider.value;
+                    AkSoundEngine.SetRTPCValue(volumeKey, masterVolume);
+                    break;
+                case "Music":
+                    musicVolume = thisSlider.value;
+                    AkSoundEngine.SetRTPCValue(volumeKey, musicVolume);
+                    break;
+                case "Sounds":
+                    SFXVolume = thisSlider.value;
+                    AkSoundEngine.SetRTPCValue(volumeKey, SFXVolume);
+                    break;
+                default:
+                    break;
+            }
+        }
 
         public void SetMusicVolume(float volume)
         {
